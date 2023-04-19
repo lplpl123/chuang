7e72992d3c37b8d35489a518f4c5d44d094c8eec
lplpl123 <758826560@qq.com>
2023-04-19 15:23:16 +0800

    202304191523

--- python_version/notes/test.py
+++ python_version/notes/test.py
@@ -1,5 +1,10 @@
-x = {}
-if x:
-    print(1)
-else:
-    print(2)
+import os
+import time
+
+
+day = time.ctime()
+print(day)
+print(type(day))
+print(len(day))
+day = day[:11] + day[20:24]
+print(day)
