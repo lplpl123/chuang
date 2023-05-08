5d1a75c64cb677047d032cbfb3429be10c087dfa
lplpl123 <758826560@qq.com>
2023-05-08 08:20:03 +0800

    202305080820

--- python_version_1.0/main.py
+++
@@ -1,9 +0,0 @@
-from tools import setup
-from surfaces.main_surface import mainSurface
-
-
-if  __name__ == '__main__':
-    root = setup.setup()
-    task_num = 5
-    mainsurface = mainSurface(root, task_num)
-    setup.run(root)
